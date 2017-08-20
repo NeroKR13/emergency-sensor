1e15cc878806dfbc25a37469fa9f5954f37b13f0
Jessica M <contact@jmvazquez.com>
2017-08-20 08:25:53 -0400

    Port

--- alert-server/index.js
+++ alert-server/index.js
@@ -6,6 +6,7 @@ var app = require('./webapp');
 
 // Create an HTTP server and listen on the configured port
 var server = http.createServer(app);
-server.listen(config.port, function() {
-  console.log('Express server listening on *:' + config.port);
+var port = config.port || process.env.port || 3000;
+server.listen(port, function() {
+  console.log('Express server listening on *:' + port);
 });
