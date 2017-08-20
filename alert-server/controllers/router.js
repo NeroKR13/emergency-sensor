6eece31446c9628d8b13983df6fdeb3dfbd52e00
Felix A. Rivas <nerokr13@gmail.com>
2017-08-20 07:32:15 -0400

    Auto stash before merge of "master" and "emergency-sensor/master"

--- alert-server/controllers/router.js
+++ alert-server/controllers/router.js
@@ -1,6 +1,8 @@
 var twilioClient = require('../twilioClient');
 var fs = require('fs');
 var admins = require('../config/administrators.json');
+var JsonDB = require('node-json-db');
+var db = new JsonDB("log", true, false);
 
 // Map routes to controller functions
 module.exports = function(router) {
@@ -9,9 +11,25 @@ module.exports = function(router) {
   });
 
   router.post('/alert', function (req, res) {
+    db.push("/alert", {
+      message:"Alert, Stranger Danger",
+      timestamp: new Date()
+    });
     admins.forEach(function(admin) {
-      var messageToSend = 'ALERT! STRANGER DANGER! :siren:';
+      var messageToSend = 'ALERT! STRANGER DANGER! :siren: @ ' + new Date();
       twilioClient.sendSms(admin.phoneNumber, messageToSend);
     });
+    res.end(200)("Alert posted");
   });
+  router.get('/alert', function(req, res) {
+    try {
+      var data = db.getData("/alert");
+      res.status(200).json(data);
+    }
+    catch(error){
+      console.error(error);
+      res.status(500).json({})
+    }
+  });
+
 };
